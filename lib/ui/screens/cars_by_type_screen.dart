a7deee4fc2bd6398f5d113490a55dd9066455e0b
dev-ajijul-islam <devajijulislam@gmail.com>
2025-12-20 20:39:54 +0600

    cars showed in cars by type from provider

--- lib/ui/screens/cars_by_type_screen.dart
+++ lib/ui/screens/cars_by_type_screen.dart
@@ -1,4 +1,7 @@
+import 'package:car_hub/providers/car_by_types_provider.dart';
+import 'package:car_hub/ui/widgets/car_card.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CarsByTypeScreen extends StatefulWidget {
   const CarsByTypeScreen({super.key});
@@ -8,10 +11,15 @@ class CarsByTypeScreen extends StatefulWidget {
 }
 
 class _CarsByTypeScreenState extends State<CarsByTypeScreen> {
-
   @override
   void initState() {
-
+    Future.microtask(() {
+      if (mounted) {
+        final String carTypeId =
+            ModalRoute.of(context)!.settings.arguments as String;
+        context.read<CarByTypesProvider>().getcarsByType(carTypeId);
+      }
+    });
     super.initState();
   }
 
@@ -20,8 +28,19 @@ class _CarsByTypeScreenState extends State<CarsByTypeScreen> {
     final String carTypeId =
         ModalRoute.of(context)!.settings.arguments as String;
     return Scaffold(
-      appBar: AppBar(title: Text("Cars by $carTypeId")),
-      body: Center(child: Text("Cars by Type")),
+      appBar: AppBar(
+        title: Text(
+          "Cars by $carTypeId (${context.watch<CarByTypesProvider>().cars.length})",
+        ),
+      ),
+      body: Consumer<CarByTypesProvider>(
+        builder: (context, provider, child) => ListView.separated(
+          padding: EdgeInsets.all(20),
+          itemBuilder: (context, index) => CarCard(car: provider.cars[index]),
+          separatorBuilder: (context, index) => SizedBox(height: 10),
+          itemCount: provider.cars.length,
+        ),
+      ),
     );
   }
 }
