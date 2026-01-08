5259f8dc84c67c316800e0ef33d7b15a7760fb6c
dev-ajijul-islam <devajijulislam@gmail.com>
2026-01-08 22:03:34 +0600

    notification screen translated

--- lib/ui/screens/home/notifications_screen.dart
+++ lib/ui/screens/home/notifications_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
 
 class NotificationsScreen extends StatelessWidget {
   const NotificationsScreen({super.key});
@@ -8,7 +9,7 @@ class NotificationsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Notifications", style: TextTheme.of(context).titleMedium),
+        title: Text("notifications.title".tr(), style: TextTheme.of(context).titleMedium),
       ),
       body: ListView.separated(
         itemBuilder: (context, index) => Dismissible(
@@ -18,16 +19,16 @@ class NotificationsScreen extends StatelessWidget {
             visualDensity: VisualDensity.adaptivePlatformDensity,
             tileColor: Colors.white,
             subtitle: Text(
-              "Your preview access to ‘Deep Work’ ends in 24 hours.",
+              "notifications.sample_message".tr(),
             ),
-            trailing: Text("1h ago"),
+            trailing: Text("notifications.time_ago".tr()),
             leading: CircleAvatar(
               backgroundColor: Colors.white,
               child: Icon(Icons.notifications, color: Colors.orange),
             ),
           ),
         ),
-        separatorBuilder: (context, index) => SizedBox(height: 5),
+        separatorBuilder: (context, index) => const SizedBox(height: 5),
         itemCount: 10,
       ),
     );
