759cc6fdaf58a2f92dfb1109d17bedfb0df69766
dev-ajijul-islam <devajijulislam@gmail.com>
2026-01-08 23:05:50 +0600

    hero banner widget translated

--- lib/ui/widgets/hero_banner.dart
+++ lib/ui/widgets/hero_banner.dart
@@ -1,7 +1,7 @@
-
 import 'package:car_hub/providers/auth_provider.dart';
 import 'package:car_hub/ui/screens/home/notifications_screen.dart';
 import 'package:car_hub/utils/assets_file_paths.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
@@ -47,23 +47,23 @@ class HeroBanner extends StatelessWidget {
                               backgroundColor: Colors.white,
                               backgroundImage: user?.photo != null
                                   ? NetworkImage(user!.photo.toString())
-                                  : AssetImage(AssetsFilePaths.dummyProfile),
+                                  : AssetImage(AssetsFilePaths.dummyProfile) as ImageProvider,
                             ),
                             Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
                                 Text(
-                                  "Hello, ${user?.name}",
+                                  "${"hero_banner.hello".tr()}, ${user?.name}",
                                   style: TextTheme.of(context).titleMedium
                                       ?.copyWith(
-                                        color: Colors.white,
-                                        fontSize: 17,
-                                        height: 0,
-                                      ),
+                                    color: Colors.white,
+                                    fontSize: 17,
+                                    height: 0,
+                                  ),
                                 ),
                                 Text(
-                                  "Welcome back!",
-                                  style: TextStyle(color: Colors.white),
+                                  "hero_banner.welcome_back".tr(),
+                                  style: const TextStyle(color: Colors.white),
                                 ),
                               ],
                             ),
@@ -81,7 +81,7 @@ class HeroBanner extends StatelessWidget {
                               NotificationsScreen.name,
                             );
                           },
-                          icon: Icon(Icons.notifications_outlined),
+                          icon: const Icon(Icons.notifications_outlined),
                         ),
                       ],
                     ),
